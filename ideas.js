0aab6fda5ecdb8c4746c363d27f6c4b560405f14
cleversh1 <cleversh@umich.edu>
2026-05-20 22:25:54 -0400

    Update ideas.js

--- ideas.js
+++ ideas.js
@@ -40,7 +40,7 @@ const IDEAS = [
     In the weeks leading up to shooting "I Fall to Pieces", my newly-appointed cinematographer was a staunch advocate of us shooting the film in ultra-widescreen (aspect ratio 2.39:1). I preferred widescreen (1.85:1).
     Weeks before the shoot turned into days before the shoot, and while I had explained that the epic, panoramic ultra-wide was ill-fitting for a domestic drama, he did not budge.
     In 1966, on the set of Mike Nichols' Who's Afraid of Virginia Woolf?, Nichols, the first-time film director, and producer-writer Ernest Lehman disagreed about whether or not Richard Burton's character should wear glasses. Nichols: for. Lehman: against.
-    Lehman asked "Well, what happens if we get very close to shooting and we still disagree?"
+    Lehman asked "Well, what happens if we get very close to shooting and we still disagree? And you think he should wear glasses and I don't?"
     And Nichols said, "Well, I'll kill you."
     I told this story to my cinematographer and he never mentioned ultra-wide again.`
   },
